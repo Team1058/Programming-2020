133a53df3c1f70411a7bf87d445b5daaa7ded386
PVC Pirate <programming@team1058.com>
2020-01-11 16:26:08 -0500

    controlmotor 1 working, file append to csv 01/11/2019

--- Programming-2020/src/main/java/frc/robot/Robot.java
+++ Programming-2020/src/main/java/frc/robot/Robot.java
@@ -6,26 +6,64 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.robot.subsystems.ColorSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+// import sun.tools.asm.CatchData;
 
 public class Robot extends TimedRobot {
 
+  ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   ColorSubsystem colorSubsystem = new ColorSubsystem();
   LEDSubsystem ledSubsystem = new LEDSubsystem();
   @Override
-  public void robotInit() {
+  public void teleopInit() {
    
-    colorSubsystem.initialize();
+    try {
+      File file = new File ("/tmp/RPM_Values.csv");
+    if(file.createNewFile()){
+      System.out.println("created");
+
+    }
+    else{
+      System.out.println("already exists");
+    }
+    PrintWriter writer = new PrintWriter("/tmp/RPM_Values.csv");
+    writer.print("");
+    writer.print("Device ID");
+    writer.print(",");
+    writer.print("RPM");
+    writer.print(",");
+    writer.print("Percent Output");
+    writer.print(",");
+    writer.print("Time");
+    writer.close();
+
+    } 
+    catch (Exception e) {
+      System.out.println("error");
+      e.printStackTrace();
+    }
+
+   // colorSubsystem.initialize();
    
   }
 
   @Override
   public void robotPeriodic() {
-    System.out.println(colorSubsystem.getColor());
-    ledSubsystem.setLEDColor(colorSubsystem.getColor());
+  }
 
+  @Override
+  public void teleopPeriodic()
+  {
+    // System.out.println(colorSubsystem.getColor());
+    // ledSubsystem.setLEDColor(colorSubsystem.getColor());
+    shooterSubsystem.Encoder();
   }
 }
