1656a4d86143f5b0f23558c58afc3e9a25fef21f
DriverStation <FryDThundor@gmail.com>
2020-01-25 13:20:48 -0500

    Finished Gyro code pushing to merge and work on combining with LEDs

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -33,6 +33,7 @@ public class DriveTrainSubsystem {
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -51,12 +52,19 @@ public class DriveTrainSubsystem{
 >>>>>>> Added ClimberSubsystem and removed unnessesary comment
 
   public void initialize() {
+<<<<<<< HEAD
     int[] leftFollower = {2};
     int[] rightFollower = {4};
     sparkMaxMotorSetLeft = new SparkMaxMotorSet(1, leftFollower);
     sparkMaxMotorSetRight = new SparkMaxMotorSet(3, rightFollower);
     drivetrain = new DifferentialDrive(sparkMaxMotorSetRight, sparkMaxMotorSetLeft, trackWidth, wheelRadius, maxWheelOmega);
     sparkMaxMotorSetRight.setInverted(true);
+=======
+    driveSparkL1 = new CANSparkMax(1, MotorType.kBrushless);
+    driveSparkL2 = new CANSparkMax(2, MotorType.kBrushless);
+    driveSparkR1 = new CANSparkMax(3, MotorType.kBrushless);
+    driveSparkR2 = new CANSparkMax(4, MotorType.kBrushless);
+>>>>>>> Finished Gyro code pushing to merge and work on combining with LEDs
 
     // Enables brake mode for all motors
     sparkMaxMotorSetLeft.setIdleMode(CANSparkMax.IdleMode.kCoast);
