0fb3fef71b112f99360d9a535045813c12b70d00
Pirate Laptop 1 <frcteam1058@gmail.com>
2020-01-25 12:24:03 -0500

    made it possible to individually adress Leds

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import frc.robot.gamepads.Gamepad;
 import frc.robot.subsystems.IndividualLeds;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
