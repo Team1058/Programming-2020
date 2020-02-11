fb55841f900235d186b408427c5a1c352bb76b2a
DriverStation <FryDThundor@gmail.com>
2020-02-10 20:42:26 -0500

    Add enable/disable. Configure position scale. :# with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXPIDSetConfiguration;
 import edu.wpi.first.wpilibj.XboxController;
@@ -64,11 +65,11 @@ public class ShooterSubsystem {
     
     updatePIDValues();
     
-    flywheel.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-    booster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    flywheel.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 10);
+    booster.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 10);
 
-    flywheel.setFeedbackCoefficientFactor(1/2048, 0, 10);
-    booster.setFeedbackCoefficientFactor(1/2048, 0, 10);
+    flywheel.configSelectedFeedbackCoefficient(1/2048, 0, 10);
+    booster.configSelectedFeedbackCoefficient(1/2048, 0, 10);
   }
 
   public void enable() {
