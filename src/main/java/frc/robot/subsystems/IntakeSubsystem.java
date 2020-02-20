7a73f012e4527535f4b144c70a8884490c663094
DriverStation <FryDThundor@gmail.com>
2020-02-19 19:57:03 -0500

    Cleaned up intake code.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,33 +9,60 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class IntakeSubsystem {
-
-    public TalonSRX intakeLift = new TalonSRX(RobotMap.CANIds.INTAKE_TALON);
-
+    private TalonSRX intakeLift = new TalonSRX(RobotMap.CANIds.INTAKE_TALON);
     private VictorSPX intakeWheels = new VictorSPX(RobotMap.CANIds.INTAKE_VICTOR_WHEELS);
+    private boolean isUp = true;
+    private double ballSpeed = -0.75;
+    double liftSlow = 0.1;
+    double liftFast = 0.3;
+    double dropSlow = -0.00;
+    double dropFast = -0.1;
 
-    //Variables Initialized
-    double motorPercentSpeed = .5;
-    double speed = 0;
+    
     public void initialize(){
-       // intakeLift.set(CANSparkMax.IdleMode.kBrake);
+       
         intakeLift.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.Disabled, 30);
         intakeLift.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.Disabled, 30);
         intakeLift.configReverseSoftLimitEnable(false);
         intakeLift.configForwardSoftLimitEnable(false);
     }
 
-    public void intakeBalls(double BallSpeed){
-        intakeWheels.set(ControlMode.PercentOutput, BallSpeed);
+    public void updateIntake(){  
+        // limit switches default to 0 when not pressed 
+        // fwd = green wire = left bumper = makes it go down
+        // rev = white wire = right bumper = makes it go up
+
+        if (intakeLift.isFwdLimitSwitchClosed() == 1 && isUp == true) {
+            intakeLift.set(ControlMode.PercentOutput, liftSlow);
+            intakeWheels.set(ControlMode.PercentOutput, 0); 
+        } else if (intakeLift.isFwdLimitSwitchClosed() == 0 && isUp == true) {
+            intakeLift.set(ControlMode.PercentOutput, liftFast);
+            intakeWheels.set(ControlMode.PercentOutput, 0); 
+        } else if (intakeLift.isRevLimitSwitchClosed() == 1 && isUp == false) {
+            intakeLift.set(ControlMode.PercentOutput, dropSlow);
+            intakeWheels.set(ControlMode.PercentOutput, ballSpeed);
+        } else if (intakeLift.isRevLimitSwitchClosed() == 0 && isUp == false) { 
+            intakeLift.set(ControlMode.PercentOutput, dropFast);
+            intakeWheels.set(ControlMode.PercentOutput, ballSpeed);
+        } else {    
+            intakeWheels.set(ControlMode.PercentOutput, 0);
+            intakeLift.set(ControlMode.PercentOutput, 0);
+        }      
+    } 
+
+    public void inferState() {
+        if (intakeLift.isFwdLimitSwitchClosed() == 1) {
+            isUp = true;
+        } else {
+            isUp = false;
+        }
     }
 
-    public void intakeOff(){
-        intakeWheels.set(ControlMode.PercentOutput, 0);
-        intakeLift.set(ControlMode.PercentOutput, 0);
-
+	public void intakeGoUp() {
+        isUp = true;
     }
-    public void liftIntake(double Speed){
-        intakeLift.set(ControlMode.PercentOutput, Speed);
+    
+    public void intakeGoDown() {
+        isUp = false;
     }
-
-}   
+}
