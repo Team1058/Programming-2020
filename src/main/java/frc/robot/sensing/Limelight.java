ddd26db89c5a8569239100f5127a67f5c48be83e
Pirate Laptop 1 <programming@team1058.com>
2020-03-10 18:11:54 -0400

    distance from ty

--- src/main/java/frc/robot/sensing/Limelight.java
+++ src/main/java/frc/robot/sensing/Limelight.java
@@ -38,7 +38,7 @@ public class Limelight {
   public void update() {
     double[] cam = camtran.getDoubleArray(new double[6]);
     txAngle = tx.getDouble(0.0);
-    tyAngle = tx.getDouble(0.0);
+    tyAngle = ty.getDouble(0.0);
     taArea = ta.getDouble(0.0);
     if (cam[1] != 0) {
       valid = true;
@@ -47,27 +47,39 @@ public class Limelight {
       distance = Math.hypot(x, y);
       simpleDistance = distance - 6;
       targetAngle = Math.atan2(-y, -x);
+
+      double k = 192;
+      double llPitch = 21.7;
+      double deltaH = 89.75;
+      double distanceFromTY = ((deltaH) / Math.tan(Math.toRadians(llPitch + tyAngle))) - 6;
+      double distanceFromTA = k / Math.sqrt(taArea) - 6;
+      double taDistance = distanceFromTA;
+      double tyDistance = distanceFromTY;
+      double avgDistance = ((distanceFromTA + distanceFromTY) / 2);
+      SmartDashboard.putNumber("ty", tyAngle);
+      SmartDashboard.putNumber("Limelight ta Distance", taDistance);
+      SmartDashboard.putNumber("Limelight ty Distance", tyDistance);
+      SmartDashboard.putNumber("Limelight avg Distance", avgDistance);
     } else {
       valid = false;
       // "k" is a magic number, like 3
       double k = 192;
-      double deltaH = (78 + 17/2 + 2) - 23;
-      double distanceFromTY = (deltaH) / 1;
+      double llPitch = 21.7;
+      double deltaH = 89.75 - 21;
+      double distanceFromTY = (deltaH) / Math.tan(Math.toRadians(llPitch + tyAngle));
       simpleDistance = k / Math.sqrt(taArea) - 6;
-      
+      double taDistance = simpleDistance;
+      double tyDistance = distanceFromTY;
+      double avgDistance = (simpleDistance + distanceFromTY) / 2;
+      SmartDashboard.putNumber("ty", tyAngle);
+      SmartDashboard.putNumber("Limelight ta Distance", taDistance);
+      SmartDashboard.putNumber("Limelight ty Distance", tyDistance);
+      SmartDashboard.putNumber("Limelight avg Distance", avgDistance);
     }
-    SmartDashboard.putNumber("simpleDistance",simpleDistance);
-    SmartDashboard.putNumber("taarea",taArea);
 
+    SmartDashboard.putNumber("Limelight simple Distance",simpleDistance);
+    SmartDashboard.putNumber("Limelight ta",taArea);
     SmartDashboard.putNumber("Limelight tx", txAngle);
-    // SmartDashboard.putNumber("X", x);
-    // //System.out.println("X: " + x);
-    // SmartDashboard.putNumber("Y", y);
-    // SmartDashboard.putNumber("Distance", distance);
-    // //System.out.println("Distance: " + distance);
-    // SmartDashboard.putNumber("Target Angle", targetAngle);
-    // System.out.println("Target Angle "+targetAngle);
-    // SmartDashboard.putBoolean("Valid", valid);
   }
 
   public double staticSimpleDistance(double distance){
